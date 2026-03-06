29b88c80a4531892a0171ab3f178e08927b068a2
BigApple96 <37613607+BigApple96@users.noreply.github.com>
2026-03-06 15:07:42 +0800

    Update __init__.py

--- plugins.v2/embysubsync/__init__.py
+++ plugins.v2/embysubsync/__init__.py
@@ -23,7 +23,7 @@ class EmbySubSync(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/refs/heads/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.3.8"
+    plugin_version = "1.3.9"
     # 插件作者
     plugin_author = "BigApple96"
     # 作者主页
